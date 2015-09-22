8569343a3bc02ab76e516b92fc9172f3dc28f710
emmanuelcu <emmanuelcu@gmail.com>
2015-09-21 22:55:40 -0500

    third commit

---
+++ TaskAppChallenge/Task.h
@@ -0,0 +1,13 @@
+//
+//  Task.h
+//  TaskAppChallenge
+//
+//  Created by Emmanuel Cuevas on 9/21/15.
+//  Copyright © 2015 Emmanuel Cuevas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Task : NSObject
+
+@end
---
+++ TaskAppChallenge/Task.m
@@ -0,0 +1,13 @@
+//
+//  Task.m
+//  TaskAppChallenge
+//
+//  Created by Emmanuel Cuevas on 9/21/15.
+//  Copyright © 2015 Emmanuel Cuevas. All rights reserved.
+//
+
+#import "Task.h"
+
+@implementation Task
+
+@end
